cfd39336640c759af04080650fafcb856fcd29c0
Sheraz10-hub <sarain0300@gmail.com>
2020-03-29 22:40:58 -0700

    Add and Remove Comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -80,6 +80,7 @@ router.get('/:id', auth, async (req, res) => {
     }
 })
 
+
 // @route    DELETE api/posts/:id
 // @desc     Delete a posts
 // @access   Private    
@@ -105,6 +106,7 @@ router.delete('/:id', auth, async (req, res) => {
     }
 })
 
+
 // @route    PUT api/posts/like/:id
 // @desc     like a posts
 // @access   Private    
@@ -113,7 +115,6 @@ router.put('/like/:id', auth, async (req, res) => {
         const post = await Post.findById(req.params.id);
         
         // check if post has been already been liked
-        // like.user: check the current
         if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
             return res.status(400).json({ msg: 'Post already liked' })
         }
@@ -130,6 +131,7 @@ router.put('/like/:id', auth, async (req, res) => {
     }
 })
 
+
 // @route    PUT api/posts/unlike/:id
 // @desc     unlike a posts
 // @access   Private    
@@ -143,10 +145,9 @@ router.put('/unlike/:id', auth, async (req, res) => {
         }
 
        // Get remove index
-       // in post model go to the likes array and map it.
        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
 
-       post.likes.splice(removeIndex, 1)  // we add one because we remove one from that
+       post.likes.splice(removeIndex, 1) 
 
         await post.save()
 
@@ -155,6 +156,83 @@ router.put('/unlike/:id', auth, async (req, res) => {
     } catch (err) {
         console.error('err.message');
         return res.status(500).send('Server Error')
+    }
+})
+
+
+
+// @route    POST api/posts/comment/:id
+// @desc     Comment on a post
+// @access   private
+
+router.post('/comment/:id', [auth,[
+    check('text', 'text must be required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+        // get the user that want to comment
+        const user = await User.findById(req.user.id).select('-password')
+
+        // get the post in which user want to comment
+        const post = await Post.findById(req.params.id)
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name, 
+            avatar: user.avatar,
+            user: req.user.id
+        }
+
+        post.comments.unshift(newComment)
+
+        await post.save();
+
+        res.json(post.comments)
+
+    } catch (err) {
+        console.error(err.message)
+        return res.status(500).send('Server Error')
+    }
+});
+
+
+// :id is the post ID
+// @route    DELETE api/posts/comment/:id/:comment_id  // :comment_id => because we need to find the post by the ID then we need to know which comment to delete.
+// @desc     Delete Comment
+// @access   private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+        // Make sure comment exists
+        if(!comment) {
+            return res.status(404).json({ msg: 'comment does not exist'})
+        }
+
+        // Check User that deleting the comment is actully that made the comment
+        if(comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'user not authorize' })
+        }
+
+        // Get remove index
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+        post.comments.splice(removeIndex, 1)
+
+        await post.save()
+
+        res.json(post.comments)
+
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
     }
 })
 
