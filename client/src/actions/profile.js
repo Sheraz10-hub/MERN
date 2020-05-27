01592e6d39d5cfc3facb78ee0fe93801139d390b
Sheraz10-hub <sarain0300@gmail.com>
2020-05-27 04:38:01 -0700

    ProfileTop & ProfileAbout

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -28,7 +28,8 @@ export const getCurrentProfile = () => async dispatch => {
 
 // Get All profile
 export const getProfiles = () => async dispatch => {
-    // when they go to the profile list page i want to clear whatever's in the current profile 
+    // when they go to the profile list page i want to clear whatever's 
+    // in the current profile 
     dispatch({ type: CLEAR_PROFILE })
 
     try {
@@ -48,7 +49,9 @@ export const getProfiles = () => async dispatch => {
 // Get profile by ID
 export const getProfileById = userId => async dispatch => {
     try {
-        const res = await axios.get(`/api/profile/${userId}`)
+        console.log('run')
+        const res = await axios.get(`/api/profile/user/${userId}`)
+        console.log(res)
         dispatch({
             type: GET_PROFILE,
             payload: res.data
@@ -220,7 +223,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
     if (window.confirm('Are you sure? This can NOT be undone!')) {
         try {
-            const res = await axios.delete('/api/profile')
+            await axios.delete('/api/profile')
 
             dispatch({ type: CLEAR_PROFILE })
             dispatch({ type: ACCOUNT_DELETED })
--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
+
+// from profile destruct user, status company etc
 const ProfileItem = ({ profile: {
     user: { _id, name, avatar },
     status,
